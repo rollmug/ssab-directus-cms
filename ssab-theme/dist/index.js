a034579cec2ff117b1460fb0bada2c0abfec9935
Dave Kobrenski <dave@bbmdesigns.com>
2023-05-22 13:39:14 -0400

    updated ssab endpoint

--- ssab-theme/dist/index.js
+++ ssab-theme/dist/index.js
@@ -1,23 +1,23 @@
 'use strict';
 
 var index = (router, { services, exceptions }) => {
-	router.get('/', (req, res) => {
-		const { ItemsService } = services;
-		const { ServiceUnavailableException } = exceptions;
+  router.get('/', (req, res) => {
+    const { ItemsService } = services;
+    const { ServiceUnavailableException } = exceptions;
 
-		const settings = new ItemsService('directus_settings', { schema: req.schema, accountability: req.accountability });
+    const settings = new ItemsService('directus_settings', { schema: req.schema, accountability: req.accountability });
 
-		settings.readByQuery({ fields: ['*'] }).then((results) => {
-            const urlBase = results[0].project_url;
-            let graphURL = new URL("graphql", urlBase);
+    settings.readByQuery({ fields: ['*'] }).then((results) => {
+      const urlBase = results[0].project_url;
+      let graphURL = new URL("graphql", urlBase);
 
-            fetch(graphURL, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    query: `
+      fetch(graphURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          query: `
                     query activeTheme {
                         activeTheme {
                           id
@@ -80,6 +80,7 @@ var index = (router, { services, exceptions }) => {
                               id
                               directus_files_id {
                                 filename_download
+                                filename_disk
                                 id
                               }
                             }
@@ -87,96 +88,104 @@ var index = (router, { services, exceptions }) => {
                         }
                       }                                            
                     `
-                })
-            }).then((response) => {
-                return response.json();
-            }).then((themeData) => {
-                const myTheme = themeData.data.activeTheme;
-                let activeTheme = myTheme.activeTheme;
-                let customLightObjects = activeTheme.customLightObjects;
-                let customDecorativeObjects = activeTheme.customDecorativeObjects;
-                let customTextures = activeTheme.customTextures;
-                let uiShapes = activeTheme.uiShapes;
+        })
+      }).then((response) => {
+        return response.json();
+      }).then((themeData) => {
+        const myTheme = themeData.data.activeTheme;
+        let activeTheme = myTheme.activeTheme;
+        let customLightObjects = activeTheme.customLightObjects;
+        let customDecorativeObjects = activeTheme.customDecorativeObjects;
+        let customTextures = activeTheme.customTextures;
+        let uiShapes = activeTheme.uiShapes;
 
-                activeTheme.lightObjects = {};
-                activeTheme.decorativeObjects = {};
-                activeTheme.textures = {};
+        activeTheme.lightObjects = {};
+        activeTheme.decorativeObjects = {};
+        activeTheme.textures = {};
 
-                delete activeTheme.customLightObjects;
-                delete activeTheme.customDecorativeObjects;
-                delete activeTheme.customTextures;
-                delete activeTheme.uiShapes;
-                activeTheme.uiShapes = [];
+        delete activeTheme.customLightObjects;
+        delete activeTheme.customDecorativeObjects;
+        delete activeTheme.customTextures;
+        delete activeTheme.uiShapes;
+        activeTheme.uiShapes = [];
 
-                if(activeTheme.backgroundImage !== null) {
-                    let bg = activeTheme.backgroundImage;
-                    activeTheme.backgroundImage.url = `${urlBase}/assets/${bg.id}/${bg.filename_download}`;
-                    delete activeTheme.backgroundImage.id;
-                    delete activeTheme.backgroundImage.filename_download;
-                }
+        if (activeTheme.colorPalette !== null) {
+          let palette = activeTheme.colorPalette.palette;
+          for (const [key, value] of Object.entries(palette)) {
+            activeTheme.colorPalette.palette[key] = `#${value}`;
+          }
+        }
 
-                if(activeTheme.hiddenObject !== null) {
-                    let bg = activeTheme.hiddenObject;
-                    activeTheme.hiddenObject.url = `${urlBase}/assets/${bg.id}/${bg.filename_download}`;
-                    delete activeTheme.hiddenObject.id;
-                    delete activeTheme.hiddenObject.filename_download;
-                }
+        if (activeTheme.backgroundImage !== null) {
+          let bg = activeTheme.backgroundImage;
+          activeTheme.backgroundImage.url = `/assets/${bg.id}/${bg.filename_download}`;
+          delete activeTheme.backgroundImage.id;
+          delete activeTheme.backgroundImage.filename_download;
+        }
 
-                if(activeTheme.cornerObject !== null) {
-                    let bg = activeTheme.cornerObject;
-                    activeTheme.cornerObject.url = `${urlBase}/assets/${bg.id}/${bg.filename_download}`;
-                    delete activeTheme.cornerObject.id;
-                    delete activeTheme.cornerObject.filename_download;
-                }
+        if (activeTheme.hiddenObject !== null) {
+          let bg = activeTheme.hiddenObject;
+          activeTheme.hiddenObject.url = `/assets/${bg.id}/${bg.filename_download}`;
+          delete activeTheme.hiddenObject.id;
+          delete activeTheme.hiddenObject.filename_download;
+        }
 
-                //light objects
-                if(typeof customLightObjects === 'object' && customLightObjects.length > 0) {
-                    customLightObjects.forEach(el => {
-                        const obj = el.clientLightObjects_id;
-                        const id = obj.objectPlacement.id;
-                        activeTheme.lightObjects[id] = `${urlBase}/assets/${obj.lightObjectImage.id}/${obj.lightObjectImage.filename_download}`;
-                    });
-                }
-                
-                // decorative objects
-                if(typeof customDecorativeObjects === 'object' && customDecorativeObjects.length > 0) {
-                    customDecorativeObjects.forEach(el => {
-                        const obj = el.clientDecorativeObjects1_id;
-                        const id = obj.objectPosition.id;
-                        activeTheme.decorativeObjects[id] = `${urlBase}/assets/${obj.decorativeObjectImage.id}/${obj.decorativeObjectImage.filename_download}`;
-                    });
-                }
+        if (activeTheme.cornerObject !== null) {
+          let bg = activeTheme.cornerObject;
+          activeTheme.cornerObject.url = `/assets/${bg.id}/${bg.filename_download}`;
+          delete activeTheme.cornerObject.id;
+          delete activeTheme.cornerObject.filename_download;
+        }
 
-                // textures
-                if(typeof customTextures === 'object' && customTextures.length > 0) {
-                    customTextures.forEach(el => {
-                        const obj = el.clientTextures_id;
-                        const id = obj.texturePlacement.id;
-                        activeTheme.textures[id] = `${urlBase}/assets/${obj.textureImage.id}/${obj.textureImage.filename_download}`;
-                    });
-                }
+        //light objects
+        if (typeof customLightObjects === 'object' && customLightObjects.length > 0) {
+          customLightObjects.forEach(el => {
+            const obj = el.clientLightObjects_id;
+            const id = obj.objectPlacement.id;
+            activeTheme.lightObjects[id] = `/assets/${obj.lightObjectImage.id}/${obj.lightObjectImage.filename_download}`;
+          });
+        }
 
-                // ui shapes
-                if(typeof uiShapes === 'object' && uiShapes.length > 0) {
-                    uiShapes.forEach(obj => {
-                        //const id = obj.id;
-                        const imgs = {
-                            "image": `${urlBase}/assets/${obj.directus_files_id.id}/${obj.directus_files_id.filename_download}?key=padding`,
-                            "hitImage": `${urlBase}/assets/${obj.directus_files_id.id}/${obj.directus_files_id.filename_download}?key=fatten`
-                        };
-                        activeTheme.uiShapes.push(imgs);
-                    });
-                }
+        // decorative objects
+        if (typeof customDecorativeObjects === 'object' && customDecorativeObjects.length > 0) {
+          customDecorativeObjects.forEach(el => {
+            const obj = el.clientDecorativeObjects1_id;
+            const id = obj.objectPosition.id;
+            activeTheme.decorativeObjects[id] = `/assets/${obj.decorativeObjectImage.id}/${obj.decorativeObjectImage.filename_download}`;
+          });
+        }
 
-                res.json(activeTheme);
-            })
-            .catch((err) => {
-                res.json(err)
-            });
-        }).catch((error) => {
-            return next(new ServiceUnavailableException(error.message));
+        // textures
+        if (typeof customTextures === 'object' && customTextures.length > 0) {
+          customTextures.forEach(el => {
+            const obj = el.clientTextures_id;
+            const id = obj.texturePlacement.id;
+            activeTheme.textures[id] = `/assets/${obj.textureImage.id}/${obj.textureImage.filename_download}`;
+          });
+        }
+
+        // ui shapes
+        if (typeof uiShapes === 'object' && uiShapes.length > 0) {
+          uiShapes.forEach(obj => {
+            //const id = obj.id;
+            const imgs = {
+              "id": obj.directus_files_id.id,
+              "image": `${urlBase}/assets/${obj.directus_files_id.filename_disk}?key=padding`,
+              "hitImage": `${urlBase}/assets/${obj.directus_files_id.id}/${obj.directus_files_id.filename_download}?key=fatten`
+            };
+            activeTheme.uiShapes.push(imgs);
+          });
+        }
+
+        res.json(activeTheme);
+      })
+        .catch((err) => {
+          res.json(err)
         });
-	});
+    }).catch((error) => {
+      return next(new ServiceUnavailableException(error.message));
+    });
+  });
 };
 
 export default index;
